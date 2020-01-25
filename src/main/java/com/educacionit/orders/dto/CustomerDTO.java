198d490e42e15a8df3c0e70dd3bd2183554e453f
EducaciónIT <jpcavallaro@gmail.com>
2020-01-25 12:20:04 -0300

    Agregando clase APP.java del profesor

---
+++ src/main/java/com/educacionit/orders/dto/CustomerDTO.java
@@ -0,0 +1,26 @@
+
+package com.educacionit.orders.dto;
+
+import lombok.*;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerDTO {
+
+    private String name;
+
+    private String lastName;
+
+    private int dni;
+
+    private String mobile;
+
+    private int age;
+
+    private String country;
+
+    private String email;
+}
